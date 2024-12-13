dff043cfac092d094a205b2342889139ee557e94
Marcin Stalmach <marcin.stalmach123@outlook.com>
2024-12-13 09:34:53 +0100

    Add functions for support subpages

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,43 @@ import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '')
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  
+  activatePage: function(pageId){
+    const thisApp = this;
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initData: function() {
     const thisApp = this;
     thisApp.data = {};
@@ -16,8 +53,8 @@ const app = {
       thisApp.data.products = parsedResponse;
       thisApp.initMenu();
     });
-    console.log('thisApp.data:', JSON.stringify(thisApp.data));
   },
+
   initMenu: function() {
     const thisApp = this;
     for (let productData in thisApp.data.products) {
@@ -33,6 +70,7 @@ const app = {
       app.cart.add(event.detail.product);
     })
   },
+
   init: function() {
     const thisApp = this;
     console.log('*** App starting ***');
@@ -40,6 +78,7 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
